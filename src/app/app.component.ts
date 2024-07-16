361e21d22cb8cfb243678c604ca085872764fec5
Diksha <malikdiksha385@gmail.com>
2024-07-16 20:46:58 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TasksComponent } from './tasks/tasks.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [TasksComponent],
+  
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'taskManager';
+  
+}
